d8cfa8fbdd9e4cd91189edf9db980f33034c0e3f
bbhoomikasb11-design <bbhoomikasb11@gmail.com>
2026-05-01 19:21:51 +0530

    functions

---
+++ functions.js
@@ -0,0 +1,22 @@
+function add(a,b) {
+    return a+b;
+}
+console.log(add(5,6));
+
+
+function isEven(num) {
+    if(num %2 == 0){
+       return "Even";
+    }
+    else{
+        return "Odd";
+    }
+
+}
+console.log(isEven(4));
+
+
+function greet(name){
+    return "Hello" + name;
+}
+console.log(greet("Bhoomika"));
