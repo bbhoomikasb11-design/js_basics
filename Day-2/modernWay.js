575fda5ed2a35cf4545243a49dcba9bfbe105c83
bbhoomikasb11-design <bbhoomikasb11@gmail.com>
2026-05-05 18:03:10 +0530

    newWay

---
+++ Day-2/modernWay.js
@@ -0,0 +1,11 @@
+const add = (a, b) => a + b; 
+
+console.log(add(5, 6)); 
+
+const isEven = (num) => num % 2 === 0 ? "Even" : "Odd";
+
+console.log(isEven(4)); 
+
+const greet = (name) => `Hello, ${name}! Welcome to the team.`;
+
+console.log(greet("Bhoomika")); // "Hello, Bhoomika! Welcome to the team."
